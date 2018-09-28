8e393409e0ce1e70be1a711a47ec7757d6cba5d7
timhagn <mail@timhagn.com>
2018-09-28 19:05:57 +0200

    update myApp.js to use dotenv

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@
 * 3. FCC Mongo & Mongoose Challenges
 * ==================================
 ***********************************************/
+/** Required for local development*/
+require('dotenv').config();
 
 /** # MONGOOSE SETUP #
 /*  ================== */
