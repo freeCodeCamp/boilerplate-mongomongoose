822d337a863c51992c12ee57f14fe923bb0f1bc2
Joshua Balogun <joshplexus8@gmail.com>
2022-06-13 18:38:47 +0100

    r

--- myApp.js
+++ myApp.js
@@ -99,8 +99,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, function (err, response) {
+    if (err) return console.log(err);
+    done(null, response);
+  });
 };
 
 const queryChain = (done) => {
