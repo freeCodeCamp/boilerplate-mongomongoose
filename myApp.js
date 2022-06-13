a802dd9869fa2aabc1e510e1801c3e721bfe210f
Joshua Balogun <joshplexus8@gmail.com>
2022-06-13 16:01:31 +0100

    d

--- myApp.js
+++ myApp.js
@@ -45,7 +45,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
