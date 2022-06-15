7f07990e059f604df321c0a9be523b1b50785513
Sr-Kamogelo <kamogelooliphant140@gmail.com>
2022-06-15 13:24:54 +0200

    change

--- myApp.js
+++ myApp.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+/** # MONGOOSE SETUP #
+/*  ================== */
+
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
 
 let Person;
