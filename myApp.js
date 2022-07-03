85f9d9ac3aeb80f72fbfae3c17d99de923448614
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-07-03 13:34:30 -0300

    moongose connection

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 
 let Person;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
