72dd9bc3dc8b104121963897c4be56fb14d0cc4a
Your Name <you@example.com>
2023-03-28 16:13:47 -0400

    l

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
-let mongoose=require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 const createAndSavePerson = (done) => {
