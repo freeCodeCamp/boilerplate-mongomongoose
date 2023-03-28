aa45d2fc6de03fc1f17d3f9855d4503a88b9c8d0
Your Name <you@example.com>
2023-03-28 16:55:16 -0400

    L

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
