44cca7de0426ee95d87da21cdf9d4212934a346c
carloz18 <carlosgmatal@gmail.com>
2023-03-28 21:01:19 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
