2e9cea9876ae16c52814c9899c976b14b8fd5ec2
artoma <artoma@gmail.com>
2023-04-11 14:09:51 +0300

    feat: add person model

--- myApp.js
+++ myApp.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 let mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
