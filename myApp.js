a9f9718058df9159b96287e1de4f4490d288e260
Walter Medrano Ruyán <waldro4513@gmail.com>
2024-03-21 20:59:07 +0000

    exercise 2 done

--- myApp.js
+++ myApp.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
 
+require('dotenv').config();
+const mongoose = require('mongoose');
 //Connect mongoose
 mongoose.connect(process.env.MONGO_URI);
 
-const Schema = mongoose.Schema;
 
-//Schema for person
-const personSchema = new Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    age: Number,
-    favoritefoods: [String]
+/** 2) Create a 'Person' Model */
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
 });
 
-const Person = mongoose.model("Person", personSchema);
+/** 3) Create and Save a Person */
+var Person = mongoose.model('Person', personSchema);
 
+var createAndSavePerson = function(done) {
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
 
 
