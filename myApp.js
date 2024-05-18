9194198d38ab0a4b4a01cf3582fe4e969b698ca7
vinycxuz <vinyby@gmail.com>
2024-05-18 13:18:40 -0300

    feat: post a .env mongo_uri and connect the server correctly

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
